ef2605500d8e1bad4b714e899aa31005a67ff321
kocierik <kocierik@gmail.com>
2023-10-16 19:58:50 +0200

    fix: contentBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,14 +36,14 @@ export default function Home() {
       <main className="flex min-h-screen	bg-base-300 flex-1  ">
         <Sidebar />
         <div className='w-full'>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
+          <div className="grid p-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
             {allSong?.length > 0 &&
               allSong?.map((songInfo, i) => {
                 return <SongItem songInfo={songInfo} key={i} />
               })
             }
           </div>
-          <div className='fixed bottom-0 bg-base-200 w-full py-2 h-[80px] px-4'>
+          <div className='sticky bottom-0 bg-base-200 w-full py-2 h-[80px] px-4'>
             <PlayerContent />
           </div>
         </div>
