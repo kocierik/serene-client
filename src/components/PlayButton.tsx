4128e0e8cb3f99ff6c8604e9e15f4fc708337b5b
kocierik <kocierik@gmail.com>
2023-10-18 16:49:43 +0200

    chore: add setting button

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -1,10 +1,8 @@
-import { songContext } from '@/providers/songContext'
-import React, { useContext } from 'react'
+import React from 'react'
 import { FaPlay } from 'react-icons/fa'
 
 const PlayButton = () => {
   return (
-    //remember how the group works, we made in the group in the song item component so when the song item is hovered over this play button renders, then when the play button itself is hovered it scales up as well
     <button  className='transition opacity-0 rounded-full flex items-center bg-success p-4 drop-shadow-md translate translate-y-1/4 group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110'>
         <FaPlay className='text-black'/>
     </button>
