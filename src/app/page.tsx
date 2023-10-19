618f500d6f5272e9755849a1878a3338ace4ada7
kocierik <kocierik@gmail.com>
2023-10-19 23:19:40 +0200

    chore: extend progress media bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,20 +23,24 @@ export default function Home() {
   }, [])
 
   return (
-      <main className="flex min-h-screen	bg-base-300 flex-1  ">
-        <Sidebar />
-        <div className='w-full'>
-          <div className="grid p-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
-            {allSong?.length > 0 &&
-              allSong?.map((songInfo, i) => {
-                return <SongItem songInfo={songInfo} setSongDescription={setSongDescription}  key={i} />
-              })
-            }
+      <main className="flex min-h-screen flex-col	bg-base-300 flex-1  ">
+        <div className='flex'>
+          <div className='flex flex-1'>
+            <Sidebar />
           </div>
+          <div className='w-full'>
+            <div className="grid p-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
+              {allSong?.length > 0 &&
+                allSong?.map((songInfo, i) => {
+                  return <SongItem songInfo={songInfo} setSongDescription={setSongDescription}  key={i} />
+                })
+              }
+            </div>
+          </div>
+        </div>
           <div className='sticky bottom-0 bg-base-200  py-2 h-[80px] px-4 '>
             <PlayerContent allSong={allSong} songDescription={songDescription} setSongDescription={setSongDescription} />
           </div>
-        </div>
       </main>
   )
 }
--- src/utils/secondToTime.ts
+++ src/utils/secondToTime.ts
@@ -2,7 +2,6 @@ function secondsToTime(seconds: number = 0) {
     const timeString = new Date(1000 * seconds)
         .toISOString()
         .slice(15, 19);
-    console.log(timeString)
     return timeString;
 }
 
